be197d366656f29730992433de43bcf664732e5e
mcaro-ro <mariocaro2002@gmail.com>
2025-07-10 20:16:01 +0200

    Pause UI now close on missing clicks

--- Assets/Scripts/UI/MenuButtonController.cs
+++ Assets/Scripts/UI/MenuButtonController.cs
@@ -1,48 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class MenuButtonController : MonoBehaviour
 {
+    [Header("Menu Content")]
     [SerializeField] private GameObject menuContent;
+    [SerializeField] private GameObject backdropMenu;
+
+    [Header("Player Controller")]
     [SerializeField] private Player player;
+
     private Button btn;
+    
+    [Header("Menu State")]
+    [SerializeField] private bool isOpen = false;
 
     private void Awake()
     {
         btn = GetComponent<Button>();
-        btn.onClick.AddListener(OpenMenu);
+        btn.onClick.AddListener(ToggleMenu);
+
+        if (menuContent != null) menuContent.SetActive(isOpen);
+        if (backdropMenu != null) backdropMenu.SetActive(isOpen);
     }
 
-    public void OpenMenu()
+    public void ToggleMenu()
     {
-        if (menuContent != null)
-        {
-            menuContent.SetActive(true);
-            Debug.Log("Menu opened");
+        isOpen = !isOpen;
+        Debug.Log("Toggling menu state: " + isOpen);
+        if (menuContent != null) menuContent.SetActive(isOpen);
+        if (backdropMenu != null)
+        { 
+            backdropMenu.SetActive(isOpen);
         }
-        if (player != null)
-            player.enabled = false;
+        if (player != null) player.enabled = !isOpen;
+        Debug.Log(isOpen ? "Menu opened" : "Menu closed");
     }
 
     public void CloseMenu()
     {
-        if (menuContent != null)
-        {
-            menuContent.SetActive(false);
-            Debug.Log("Menu closed");
-        }
+        if (!isOpen) return;
 
-        if (player  != null)
-            player.enabled = true;
+        isOpen = false;
+
+        if (menuContent != null) menuContent.SetActive(false);
+        if (backdropMenu != null) backdropMenu.SetActive(false);
+        if (player  != null) player.enabled = true;
     }
 
     private void OnDestroy()
     {
         if (btn != null)
         {
-            btn.onClick.RemoveListener(OpenMenu);
+            btn.onClick.RemoveListener(ToggleMenu);
         }
     }
 }
