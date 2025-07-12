82d7d8254196a1c15cfce29791b98e0aabbae1b7
Manuel Galvez <60622528+manuelgalvez192@users.noreply.github.com>
2025-07-12 21:44:19 +0200

    levels icons update

--- Assets/Scripts/UI/LevelBtnManager.cs
+++ Assets/Scripts/UI/LevelBtnManager.cs
@@ -7,6 +7,9 @@ public class LevelBtnsManager : MonoBehaviour
     [Header("Level Manager")]
     [SerializeField] private LevelManager levelManagerScript;
     [SerializeField] private MenuButtonController menuButtonController;
+    [Header("Sprites")]
+    [SerializeField] private Sprite levelUnlockedSprite;
+    [SerializeField] private Sprite levelLockedSprite;
     
     public Level levelData;
     public Button button;
@@ -23,9 +26,9 @@ public class LevelBtnsManager : MonoBehaviour
         isUnlocked = LevelProgressManager.IsLevelUnlocked(levelData.id);
         button.interactable = isUnlocked;
         if (isUnlocked)
-            button.image.color = new Color(0, 1, 0, 1f);
+            button.image.sprite = levelLockedSprite;
         if (!isUnlocked)
-            button.image.color = new Color(1, 0, 0, 1f);
+            button.image.sprite = levelUnlockedSprite;
     }
     
     public void LoadLevel(int levelID)
